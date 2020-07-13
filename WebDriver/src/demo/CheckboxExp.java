a78a13f285c3b127fb0c57a47a0dea0409e64e45
hiren <hiren@192.168.0.22>
2020-07-12 22:49:33 -0400

    pushing new code

--- WebDriver/src/demo/CheckboxExp.java
+++ WebDriver/src/demo/CheckboxExp.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CheckboxExp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		
 		System.setProperty("webdriver.chrome,driver","chromedriver.exe");
